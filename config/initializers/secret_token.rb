ff3c45191f63fbe96b33724177e409a53620645a
Ross Hochwert <rosshochwert@gmail.com>
2013-03-27 11:01:17 -0500

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eventfinder::Application.config.secret_token = '0a6f7ae90e61f1d09ec4001b68299033b43f631fdc110f917a6aac8962255939eb8d075a1a87c639e35b64e3ed8f00b982068a80354e1a2b0652e14b56a33f9d'
