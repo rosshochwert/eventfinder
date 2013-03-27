daa52852852d76353e0456c62b14aeef427e1e57
Ross Hochwert <rosshochwert@gmail.com>
2013-03-27 15:42:56 -0500

    did anyting happen

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130327194956) do
+ActiveRecord::Schema.define(:version => 20130327202541) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -19,10 +19,11 @@ ActiveRecord::Schema.define(:version => 20130327194956) do
     t.string   "provider"
     t.string   "uid"
     t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
     t.string   "first_name"
     t.string   "user_image"
+    t.string   "fb_access_token"
   end
 
 end
