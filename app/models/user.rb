90d4f762c633c32b34c7ffe28de824b7afe8a448
Ross Hochwert <rosshochwert@gmail.com>
2013-03-27 14:36:41 -0500

    added user stuff fjg

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ def self.create_with_omniauth(auth)
 	create! do |user|
 		user.provider = auth["provider"]
 		user.uid = auth["uid"]
-	#	user.fb_access_token = auth['credentials']['token']
+		user.fb_access_token = auth['credentials']['token']
         user.name = auth["info"]["name"]
         user.first_name = auth["info"]["first_name"]
-    #   user.image = auth["info"]["image"]
+        user.image = auth["info"]["image"]
     end
 end
 
